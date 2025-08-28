8b793848344738e88177143b2a5f307a1f77a9b4
kentafar1k <129420782+kentafar1k@users.noreply.github.com>
2025-08-28 13:21:20 +0300

    сменил метод load_model

--- voice-to-text-sobes-main/transcribe.py
+++ voice-to-text-sobes-main/transcribe.py
@@ -1,6 +1,6 @@
 import whisper
 
-model = whisper.load_model("medium")  # можно заменить на "small" или "large"
+model = whisper.load_model("small")  # можно заменить на "small" или "large" или как было "medium"
 result = model.transcribe("interview.m4a", language="ru")
 
 with open("result.txt", "w", encoding="utf-8") as f:

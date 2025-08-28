959a14be0efdcb8c4a57957b1569373cd5d88609
kentafar1k <129420782+kentafar1k@users.noreply.github.com>
2025-08-28 13:36:58 +0300

    добавил явное указание пути в path

--- voice-to-text-sobes-main/transcribe.py
+++ voice-to-text-sobes-main/transcribe.py
@@ -1,4 +1,9 @@
 import whisper
+import os
+
+
+# явное указание пути в path
+os.environ['PATH'] = r"E:\PROGRAMS\ffmpeg\ffmpeg-2025-08-25-git-1b62f9d3ae-essentials_build\bin;" + os.environ['PATH']
 
 model = whisper.load_model("small")  # можно заменить на "small" или "large" или как было "medium"
 result = model.transcribe("interview.m4a", language="ru")
